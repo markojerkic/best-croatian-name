2540982abf98181a68897b06a3c5e8f29624ad99
Marko Jerkić <markojerkic266@gmail.com>
2022-04-03 00:21:33 +0200

    prisma local mysql connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,71 @@
+import { names } from '@prisma/client';
 import type { NextPage } from 'next'
 import { useState } from 'react';
+import { prisma } from '../backend/index';
 
 type NameSelection = {
-  firstName: string;
-  secondName: string;
+  firstName: names;
+  secondName: names;
 }
 
-const names = [
+const namesExampels = [
   'Marko',
   'Ivan',
   'Karlo',
   'Luka'
 ]
 
-const NameChoice: React.FC<{name: string; onClick: () => void}> = ({name, onClick}) => {
+const NameChoice: React.FC<{name: string}> = ({name}) => {
   return (
     <div className="w-64 h-64 flex justify-center items-center 
       hover:bg-red-500 bg-red-300 hover:cursor-pointer rounded-md
       font-bold"
-      onClick={() => onClick()}>{name}</div>
+      // onClick={() => onClick()}
+      >{name}</div>
   );
 }
 
 const randomNames = () => {
-  const firstName =  names[Math.floor(Math.random()*names.length)];
-  const secondName = names[Math.floor(Math.random()*names.length)];
+  const firstName =  namesExampels[Math.floor(Math.random()*namesExampels.length)];
+  const secondName = namesExampels[Math.floor(Math.random()*namesExampels.length)];
   return {firstName: firstName, secondName: secondName};
 }
 
-const Home: NextPage = () => {
-  const [names, setNames] = useState<NameSelection>(randomNames())
+const Home: NextPage<{nameSelection: NameSelection}> = ({nameSelection}) => {
+  const [{firstName, secondName}, setNames] = useState<NameSelection>(nameSelection)
   return (
    <div className="w-full h-screen space-x-4 flex justify-center items-center">
-     <NameChoice name={names.firstName} onClick={() => setNames(randomNames())} />
+     <NameChoice name={firstName.name} 
+    //  onClick={() => setNames(randomNames())} 
+     />
      <div>ili</div>
-     <NameChoice name={names.secondName} onClick={() => setNames(randomNames())} />
+     <NameChoice name={secondName.name} 
+    //  onClick={() => setNames(randomNames())} 
+     />
    </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const maleOrFemale = Math.floor(Math.random() * 2) == 0? 'male': 'female';
+  const minMax = await prisma.names.aggregate({
+    _min: {id: true},
+    _max: {id: true},
+    where: { gender: maleOrFemale }
+  });
+  const generate = () => Math.floor(Math.random() * ((minMax._max.id || 1)-(minMax._min.id || 1))) + (minMax._min.id || 1)-1;
+  const ids = [generate(), generate(), generate()]
+  const names = await prisma.names.findMany({
+    where: { id: {in: ids } }
+  });
+  return {
+    props: {
+      nameSelection: {
+        firstName: names[0],
+        secondName: names[1]
+      }
+    }
+  }
+}
+
+export default Home;
