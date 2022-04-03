15f629db199bf88a619e2d5697aa62dd97ecbcb9
Marko Jerkić <markojerkic266@gmail.com>
2022-04-03 18:03:24 +0200

    load new names on click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
+import NameSelection from '@/types/name-selection';
 import { names, names_gender } from '@prisma/client';
 import axios from 'axios';
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-type NameSelection = {
-  firstName: names;
-  secondName: names;
+const randomNames = async (setNames: Dispatch<SetStateAction<NameSelection>>) => {
+  setNames(await axios.get<NameSelection>('/api/random-names')
+  .then(response => response.data));
 }
 
-const NameChoice: React.FC<{name: string, gender: names_gender}> = ({name, gender}) => {
+const NameChoice: React.FC<{name: string, gender: names_gender, click: () => void}> = ({name, gender, click}) => {
   const maleOfFemale = gender === 'male'? 'hover:bg-blue-500 bg-blue-300': 'hover:bg-red-500 bg-red-300';
   const className = `w-64 h-64 flex justify-center items-center 
     hover:cursor-pointer rounded-md
     font-bold ${maleOfFemale}`;
   return (
-    <div className={className}
-      // onClick={() => onClick()}
-      >{name}</div>
+    <button className={"btn btn-lg " + className}
+      onClick={() => click()}
+      >{name}</button>
   );
 }
 
@@ -25,11 +26,11 @@ const Home: NextPage<{nameSelection: NameSelection}> = ({nameSelection}) => {
   return (
    <div className="w-full h-screen space-x-4 flex justify-center items-center">
      <NameChoice name={firstName.name} gender={firstName.gender}
-    //  onClick={() => setNames(randomNames())} 
+     click={() => randomNames(setNames)} 
      />
      <div>ili</div>
      <NameChoice name={secondName.name} gender={secondName.gender}
-    //  onClick={() => setNames(randomNames())} 
+     click={() => randomNames(setNames)} 
      />
    </div>
   )
