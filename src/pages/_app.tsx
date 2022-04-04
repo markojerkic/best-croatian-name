92d20d55bbb0309f02be4816ed9350b6dd96adb2
Marko Jerkić <markojerkic266@gmail.com>
2022-04-04 20:07:14 +0200

    name and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>Odabir najljepših hrvatskih imena</title>
+        <meta property="og:title" content="Odabir najljepših hrvatskih imena<" key="title" />
+        <link rel="shortcut icon" href="/favicon_kockice.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
