db797f53601ed3b1b9b09dde1e6e711fc1ec4c11
Marko Jerkić <markojerkic266@gmail.com>
2022-04-06 19:38:14 +0200

    lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Navbar from '../components/navbar'
-import Head from 'next/head'
-import React from 'react'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import Navbar from '../components/navbar';
+import Head from 'next/head';
+import React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({Component, pageProps}: AppProps) => {
   return (
     <div>
       <Head>
         <title>Odabir najljepših hrvatskih imena</title>
-        <meta property="og:title" content="Odabir najljepših hrvatskih imena" key="title" />
+        <meta property="og:title"
+          content="Odabir najljepših hrvatskih imena" key="title" />
         <link rel="shortcut icon" href="/favicon_kockice.ico" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
     </div>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
